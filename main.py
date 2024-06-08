b30b7b6b79d5183498168102f6259215021c2567
Mantudash <kumarmantudash@gmail.com>
2024-06-08 14:01:08 +0530

    Mantu

--- main.py
+++ main.py
@@ -1,1 +1,3 @@
-import fastapi
+from fastapi import *
+app = FastAPI(title="E-commerce API", version="0.1.1",
+              description=" E-commerce API created with FastAPI and jwt Authenticated")
