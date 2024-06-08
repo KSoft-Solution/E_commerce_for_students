90ee439110c96cebb9fba18748d53723cb85976f
ramapatro <psubhalaxmipatro@gmail.com>
2024-06-08 13:42:59 +0530

    subhalaxmi_changes

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-import fastapi
+import fastapi   #use this for python modules(os,date time)
+
+from fastapi import FastAPI  #use this way for third party libraries(numpy,pandas,fastapi)
+
+from fastapi import *  #use this way for userdefined modules
+
+fastapi.FastAPI()
+
+app = FastAPI(title="E-commerce API", version="0.1.1",
+            description=" E-commerce API created with FastAPI and jwt Authenticated")
