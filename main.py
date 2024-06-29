d021accf928d3c86e64f0087269cc4485ff4df62
KSoft-Solution <kanhasahu955902@gmail.com>
2024-06-29 21:35:31 +0530

    api setup routing

--- main.py
+++ main.py
@@ -1,48 +1,17 @@
 # import uvicorn
-import datetime
-from fastapi import FastAPI,status
-from fastapi.responses import JSONResponse
-from fastapi.encoders import jsonable_encoder
+from fastapi import FastAPI
+from config.config import PROJECT_NAME
 
-from pydantic import BaseModel
-
-from config.config import PORT,PROJECT_NAME
-# import config.db
+from routes.user import router
 
 app = FastAPI(title=PROJECT_NAME, version="0.1.1",
               description=" E-commerce API created with FastAPI and jwt Authenticated")
-
-class Response(BaseModel):
-    # timestamp: datetime
-    response:dict
-    status:int
-    error:bool
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-@app.get('/my_profile')
-def get_my_name() :
-    profile:dict = { 
-        "name":"ashok sahu",
-        "age":29,
-        "job":"software engineer",
-        "friends":['arup','gopal']
-    }
-    
-    response:Response = {
-        "status":status.HTTP_200_OK,
-        "error":False,
-        "response":profile,
-        # "timestamp":datetime
-    }
-    
-    data = jsonable_encoder(response)
-    
-    return JSONResponse(content=data)
-    
-
+app.include_router(router)
 # if __name__ == '__main__':
 #     uvicorn.run('main:app', port=6000, reload=True)
 
---
+++ routes/user.py
@@ -0,0 +1,26 @@
+from fastapi import APIRouter,status
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+
+from models.user import UserResponse
+
+router = APIRouter()
+
+@router.get('/my_profile')
+def get_my_name() :
+    profile:dict = { 
+        "name":"ashok sahu",
+        "age":29,
+        "job":"software engineer",
+        "friends":['arup','gopal']
+    }
+    
+    response:UserResponse = {
+        "status":status.HTTP_200_OK,
+        "error":False,
+        "response":profile,
+    }
+    
+    data = jsonable_encoder(response)
+    
+    return JSONResponse(content=data)
