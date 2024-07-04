f2c662165d3630ab5bc223f674ada362e20d40bd
Soubhagya kumar Nayak <sibaninayak@example.com>
2024-07-04 21:58:33 +0530

    sibani

--- routes/product.py
+++ routes/product.py
@@ -1,9 +1,16 @@
 from fastapi import APIRouter
+from models.product import Product
 
-
-product_router = APIRouter()
+product_router = APIRouter(prefix="/product",tags=['product'])
 
 @product_router.get('/samsung')
 def samsung():
     return {'samsung': 'samsung'}
+
+@product_router.post('/create')
+def create_product(data:Product):
+    print(data)
+    return {
+        'response': "success"
+    }
 
